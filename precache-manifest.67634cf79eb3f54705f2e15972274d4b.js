5456c18c660c3da5b213c751d24ae296df877bc5
asha <arunk7839@gmail.com>
2018-12-10 19:10:34 +0530

    Updates

---
+++ precache-manifest.67634cf79eb3f54705f2e15972274d4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "6917b5c99af711778923",
+    "url": "/robofriends/static/js/main.6917b5c9.chunk.js"
+  },
+  {
+    "revision": "be373ff23e2424078ce8",
+    "url": "/robofriends/static/js/1.be373ff2.chunk.js"
+  },
+  {
+    "revision": "6917b5c99af711778923",
+    "url": "/robofriends/static/css/main.5579dc44.chunk.css"
+  },
+  {
+    "revision": "be373ff23e2424078ce8",
+    "url": "/robofriends/static/css/1.45005e43.chunk.css"
+  },
+  {
+    "revision": "6b4181f2c1321a4376aa1c503de6d351",
+    "url": "/robofriends/index.html"
+  }
+];
